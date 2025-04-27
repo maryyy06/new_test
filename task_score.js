05161a2df3c765542df8aed9e4f07f5d788fac4c
Maryyy06 <miss-mary_13@mail.ru>
2025-04-27 21:36:45 +0300

    Homework

---
+++ task_score.js
@@ -0,0 +1,8 @@
+let introduction = 90,
+   git = 269,
+   js = 120;
+ let averageScore = (introduction + git + js) / 3;
+ let studentFirstName = "Marina";
+ studentLastName = "Kozlova";
+ 
+ console.log(`The average score of the student ${studentFirstName} ${studentLastName} is ${averageScore.toFixed(2)}`);
