53a63d0b58e2df6045f5d5042ecf41de43388c4a
Maryyy06 <miss-mary_13@mail.ru>
2025-05-05 16:21:44 +0300

    Add  Increment and decrement in function

--- calculation.js
+++ calculation.js
@@ -15,8 +15,24 @@ function increaseBalance(x, y) {
   function getRestAfterDivision(x, y) {
     return x %= y
   }
+
+  function incrementBalance (x) {
+    return (result = ++x) //Префиксная — оператор ставится перед операндом.
+  }
   
+  function decrementBalance (x) {
+    return (ressult = --x) //Префиксная — оператор ставится перед операндом.
+  }
+
+  function postIncrementBalance (x) {
+    return (ressult = x++) 
+  } //Постфиксная  — оператор ставится после опернада
+
+
   console.log(increaseBalance(3000, 700));
   console.log(decreaseBalance(3000, 700));
   console.log(divideBalanceByAccounts(3000, 2));
   console.log(getRestAfterDivision(7000, 3))
+  console.log(incrementBalance (10))
+  console.log(decrementBalance (10))
+  console.log(postIncrementBalance (15))
