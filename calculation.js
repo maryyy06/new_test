d531830b417f8068f849ff1bd6f285123826677f
Maryyy06 <miss-mary_13@mail.ru>
2025-05-05 13:51:06 +0300

    corrected code

--- calculation.js
+++ calculation.js
@@ -23,4 +23,4 @@ function increaseBalance(x, y) {
   console.log(increaseBalance(3000, 700));
   console.log(decreaseBalance(3000, 700));
   console.log(divideBalanceByAccounts(3000, 2));
-  console.log(getRestAfterDivision(7000, 3));
+  console.log(getRestAfterDivision(7000, 3))
