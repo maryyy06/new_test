9fa3875829d69260733030bb15abd3b0291cd45f
Maryyy06 <miss-mary_13@mail.ru>
2025-05-05 12:49:49 +0300

    update

---
+++ functoin.js
@@ -0,0 +1,14 @@
+function logUserName(UserName) {
+    console.log (UserName)
+}
+
+logUserName ("Oksana")
+
+let x = "text"
+console.log ("text".length)
+
+function showMessage() {
+    alert( 'Всем привет!' );
+  }
+  
+  showMessage('Всем привет!');
